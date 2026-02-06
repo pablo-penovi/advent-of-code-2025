917e9b3bd474a99d89e45e15d56c5b0f174926f3
Pablo Penovi <pablo.penovi@gmail.com>
2026-02-06 15:33:42 -0300

    first commit

---
+++ .zig-cache/o/fee079de67c54e7176522200a7792fd0/dependencies.zig
@@ -0,0 +1,12 @@
+pub const packages = struct {
+    pub const @"tui-0.0.1-TMwxp-tWBgBwZqdkgqHn4c_YDJSUQhha2-OKOFBGYig3" = struct {
+        pub const build_root = "/home/pablo/.cache/zig/p/tui-0.0.1-TMwxp-tWBgBwZqdkgqHn4c_YDJSUQhha2-OKOFBGYig3";
+        pub const build_zig = @import("tui-0.0.1-TMwxp-tWBgBwZqdkgqHn4c_YDJSUQhha2-OKOFBGYig3");
+        pub const deps: []const struct { []const u8, []const u8 } = &.{
+        };
+    };
+};
+
+pub const root_deps: []const struct { []const u8, []const u8 } = &.{
+    .{ "tui", "tui-0.0.1-TMwxp-tWBgBwZqdkgqHn4c_YDJSUQhha2-OKOFBGYig3" },
+};
--- build.zig
+++ build.zig
@@ -7,41 +7,78 @@ const std = @import("std");
 // build runner to parallelize the build automatically (and the cache system to
 // know when a step doesn't need to be re-run).
 pub fn build(b: *std.Build) void {
+    // Standard target options allow the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
+    // It's also possible to define more custom flags to toggle optional features
+    // of this build script using `b.option()`. All defined flags (including
+    // target and optimize options) will be listed when running `zig build --help`
+    // in this directory.
 
-    // Core module - shared utilities and business logic
-    const core_module = b.addModule("core", .{
-        .root_source_file = b.path("src/core/root.zig"),
+    // This creates a module, which represents a collection of source files alongside
+    // some compilation options, such as optimization mode and linked system libraries.
+    // Zig modules are the preferred way of making Zig code available to consumers.
+    // addModule defines a module that we intend to make available for importing
+    // to our consumers. We must give it a name because a Zig package can expose
+    // multiple modules and consumers will need to be able to specify which
+    // module they want to access.
+    const mod = b.addModule("aoc2026", .{
+        // The root source file is the "entry point" of this module. Users of
+        // this module will only be able to access public declarations contained
+        // in this file, which means that if you have declarations that you
+        // intend to expose to consumers that were defined in other files part
+        // of this module, you will have to make sure to re-export them from
+        // the root file.
+        .root_source_file = b.path("src/root.zig"),
+        // Later on we'll use this module as the root module of a test executable
+        // which requires us to specify a target.
         .target = target,
-        .optimize = optimize,
     });
 
-    // TUI module - user interface components
-    const tui_module = b.addModule("tui", .{
-        .root_source_file = b.path("src/tui/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    tui_module.addImport("core", core_module);
-
-    // Add external TUI.zig dependency
-    const tui_dep = b.dependency("tui", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    tui_module.addImport("tui_zig", tui_dep.module("tui"));
-
-    // Main executable
+    // Here we define an executable. An executable needs to have a root module
+    // which needs to expose a `main` function. While we could add a main function
+    // to the module defined above, it's sometimes preferable to split business
+    // logic and the CLI into two separate modules.
+    //
+    // If your goal is to create a Zig library for others to use, consider if
+    // it might benefit from also exposing a CLI tool. A parser library for a
+    // data serialization format could also bundle a CLI syntax checker, for example.
+    //
+    // If instead your goal is to create an executable, consider if users might
+    // be interested in also being able to embed the core functionality of your
+    // program in their own executable in order to avoid the overhead involved in
+    // subprocessing your CLI tool.
+    //
+    // If neither case applies to you, feel free to delete the declaration you
+    // don't need and to put everything under a single module.
     const exe = b.addExecutable(.{
         .name = "aoc2026",
         .root_module = b.createModule(.{
+            // b.createModule defines a new module just like b.addModule but,
+            // unlike b.addModule, it does not expose the module to consumers of
+            // this package, which is why in this case we don't have to give it a name.
             .root_source_file = b.path("src/main.zig"),
+            // Target and optimization levels must be explicitly wired in when
+            // defining an executable or library (in the root module), and you
+            // can also hardcode a specific target for an executable or library
+            // definition if desireable (e.g. firmware for embedded devices).
             .target = target,
             .optimize = optimize,
+            // List of modules available for import in source files part of the
+            // root module.
             .imports = &.{
-                .{ .name = "core", .module = core_module },
-                .{ .name = "tui", .module = tui_module },
+                // Here "aoc2026" is the name you will use in your source code to
+                // import this module (e.g. `@import("aoc2026")`). The name is
+                // repeated because you are allowed to rename your imports, which
+                // can be extremely useful in case of collisions (which can happen
+                // importing modules from different packages).
+                .{ .name = "aoc2026", .module = mod },
             },
         }),
     });
@@ -78,35 +115,31 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    // Creates an executable that will run `test` blocks from the core module.
-    const core_tests = b.addTest(.{
-        .root_module = core_module,
+    // Creates an executable that will run `test` blocks from the provided module.
+    // Here `mod` needs to define a target, which is why earlier we made sure to
+    // set the releative field.
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
     });
 
-    // A run step that will run the core test executable.
-    const run_core_tests = b.addRunArtifact(core_tests);
-
-    // Creates an executable that will run `test` blocks from the TUI module.
-    const tui_tests = b.addTest(.{
-        .root_module = tui_module,
-    });
-
-    // A run step that will run the TUI test executable.
-    const run_tui_tests = b.addRunArtifact(tui_tests);
+    // A run step that will run the test executable.
+    const run_mod_tests = b.addRunArtifact(mod_tests);
 
     // Creates an executable that will run `test` blocks from the executable's
-    // root module.
+    // root module. Note that test executables only test one module at a time,
+    // hence why we have to create two separate ones.
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
 
-    // A run step that will run the main executable test executable.
+    // A run step that will run the second test executable.
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
-    // A top level step for running all tests.
+    // A top level step for running all tests. dependOn can be called multiple
+    // times and since the two run steps do not depend on one another, this will
+    // make the two of them run in parallel.
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_core_tests.step);
-    test_step.dependOn(&run_tui_tests.step);
+    test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
 
     // Just like flags, top level steps are also listed in the `--help` menu.
